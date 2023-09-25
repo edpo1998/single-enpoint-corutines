f36e8d57203fc8d1ec6b745a8b11f9f008915edb
edpo1998 <116203240+edannyp@users.noreply.github.com>
2023-09-25 14:40:47 -0600

    FIX: Expose container app

--- app.py
+++ app.py
@@ -2,14 +2,12 @@
 from flask import Flask
 from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app=app, resources={ r"/api/v1/*": {"origins": "*"} })
+from apis import api
+api.init_app(app=app)
 
-def make_app():
-    app = Flask(__name__)
-    cors = CORS(app=app, resources={ r"/api/v1/*": {"origins": "*"} })
-    from apis import api
-    api.init_app(app=app)
-    return app
 
 if __name__ == '__main__':
-    app = make_app()
-    app.run(port=8000,host='0.0.0.0')
+
+    app.run(port=8000)
